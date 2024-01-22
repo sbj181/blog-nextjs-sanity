1e8d4abd92d1607368e3b695abea0897c9caa3b8
Scott Johnson <scottj@thegrovery.com>
2024-01-22 12:14:38 -0500

    removing hamburger menu

--- components/BlogHeader.tsx
+++ components/BlogHeader.tsx
@@ -87,42 +87,7 @@ export default function BlogHeader({
           label={darkMode ? 'Switch to Light Mode' : 'Switch to Dark Mode'}
         />
       </nav>
-      <Disclosure as="nav" className="">
-        {({ open }) => (
-          <Fragment>
-            <div className="px-2 mx-auto max-w-7xl sm:px-4 lg:px-8">
-              <div className="relative flex items-center justify-between h-16">
-                {/* ...logo or title */}
-                <div className="-mr-2 flex">
-                  <Disclosure.Button className="inline-flex ml-auto items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                    <span className="sr-only">Open main menu</span>
-                    {open ? (
-                      <XIcon className="block h-6 w-6" aria-hidden="true" />
-                    ) : (
-                      <MenuIcon className="block h-6 w-6" aria-hidden="true" />
-                    )}
-                  </Disclosure.Button>
-                </div>
-              </div>
-            </div>
-            <Disclosure.Panel className="absolute z-10 bg-white w-1/3 shadow-lg right-5 dark:bg-gray-800">
-              <div className="px-2 pt-2 pb-3 space-y-1">
-                {navigation.map((item) => (
-                  <Link
-                  className={item.current ? 'bg-red-800 text-white block px-3 py-2 rounded-md text-base font-medium' : 'text-gray-500 dark:text-gray-50 block px-3 py-2 rounded-md text-base font-medium'}
-                    key={item.name}
-                    href={item.href}
-                  >
-                    
-                      {item.name}
-                    
-                  </Link>
-                ))}
-              </div>
-            </Disclosure.Panel>
-          </Fragment>
-        )}
-      </Disclosure>
+      
       
     </header>
   );
